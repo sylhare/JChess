77095213d0834ca591e739a3c66bf99a66686108
sylhare <sylhareng@gmail.com>
2018-02-08 15:58:48 -0500

    adding board test

---
+++ src/main/java/com/company/BoardTest.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sylhare in 2017.
+ */
+public class BoardTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void printChessDemo() throws Exception {
+    }
+
+    @org.junit.Test
+    public void scanCommand() throws Exception {
+    }
+
+    @org.junit.Test
+    public void parse() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isCoordinate() throws Exception {
+    }
+
+    @org.junit.Test
+    public void move() throws Exception {
+    }
+
+}
