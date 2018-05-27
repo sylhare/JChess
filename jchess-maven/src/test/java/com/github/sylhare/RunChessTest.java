a868216c0f20bb09cc3beac36d0690c8aab1928f
sylhare <sylhareng@gmail.com>
2018-05-26 20:32:05 -0400

    Update repository structure

--- jchess-maven/src/test/java/com/github/sylhare/RunChessTest.java
+++ jchess-maven/src/test/java/com/github/sylhare/RunChessTest.java
@@ -5,7 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"},
-                 features = { "./src/test/resources/com.github.sylhare/" })
+@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
 public class RunChessTest {
 }
